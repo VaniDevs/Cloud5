00d7b2e686ebf80afbe0123c8c014eab94191910
Shiv <shivbirr@gmail.com>
2018-09-23 10:59:42 -0700

    ui change

--- Controllers/scripts/login.js
+++ Controllers/scripts/login.js
@@ -9,6 +9,7 @@ var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().signInWithPopup(provider).then(function(result) {
    token = result.credential.accessToken;
    user = result.user;
+   sessionStorage.setItem('usertoken', token);
  
 }).catch(function(error) {
   var errorCode = error.code;
@@ -16,5 +17,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   var email = error.email;
   var credential = error.credential;
 });
+
+
 }
 
