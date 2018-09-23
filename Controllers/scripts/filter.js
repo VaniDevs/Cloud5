4a5a58051d4bd7123d7701657a4653a88cf17e9a
Edmund Ham <hamsu2959@gmail.com>
2018-09-23 03:57:26 -0700

    finished search and filter function (#8)
    * added search
    
    * removed test in the name
    
    * removed error part
    
    * added hash and timestamp function
    
    * finished search and filter function

--- Controllers/scripts/filter.js
+++ Controllers/scripts/filter.js
@@ -1,41 +1,68 @@
-const database = firebase.database();
-
 function updateServiceList() {
-    var aServiceList = search();
+    // deleteAllPanel();
 
-    deleteAllPanel();
+    let aServiceList = search();
 
     aServiceList.forEach(function(sService) {
         generateServicePanel(sService);
     });
 }
 
-function updateAgeFilter() {
+function updateFilter() {
+    // deleteAllPanel();
+
+    let ageInput = $('#ageInput').val();
+    let cityInput = $('#cityInput').val();
+    let genderInput = $('input[name="genderInput"]:checked').val();
+
+    var aServiceList = [];
+    var aServiceContent = [];
+    var oService = {};
+    var aFilteredService = [];
     
-}
+    let ref = database.ref("service/");
+    ref.on('value', function(snapshot) {
+        if (snapshot.val()) {
+            // console.log(snapshot.val());
+            aServiceList = Object.keys(snapshot.val());
+            aServiceContent = Object.values(snapshot.val());
+            for (var i = 0; i < aServiceList.length; i++) {
+                oService[aServiceList[i]] = aServiceContent[i];
+            }
+        }
+    });
 
-function updateCityFilter() {
+    for (var key in oService) {
+        if (oService[key]["city"] == cityInput) {
+            aFilteredService.push(key);
+        } else if (oService[key]["gender"] == "all") {
+            aFilteredService.push(key);
+        } else if (oService[key]["gender"] == genderInput) {
+            aFilteredService.push(key);
+        } else if (oService[key]["ageFrom"] < ageInput && oService[key]["ageTo"] > ageInput) {
+            aFilteredService.push(key);
+        }
+    }
 
-}
-
-function updateGenderFilter() {
-
+    aFilteredService.forEach(function(serviceId) {
+        generateServicePanel(serviceId);
+    })
 }
 
 function search() {
-    // let sSearchTocken = $('#placeholder').value;
-    var sSearchTocken = "Sport sport";
-    var aSearchTocken = sSearchTocken.split(' ');
+    let sSearchTocken = $('#searchInput').val();
+    // let sSearchTocken = "Sport sport";
+    let aSearchTocken = sSearchTocken.split(' ');
 
-    var aServiceList = [];
+    let aServiceList = [];
 
     aSearchTocken.forEach(function(sTocken) {
-        var ref = database.ref("keyWords/" + sTocken);
+        let ref = database.ref("keyWords/" + sTocken);
         ref.on('value', function(snapshot) {
             if (snapshot.val()) {
                 let tempServiceList = Object.keys(snapshot.val());
                 tempServiceList.forEach(function(service) {
-                    console.log(service);
+                    // console.log(service);
                     aServiceList.push(service);
                 });
             }
@@ -43,6 +70,3 @@ function search() {
     });
     return aServiceList;
 }
-
-
-
