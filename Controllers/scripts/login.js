7cac478dba85138cab71cf86f494a275c4b660b6
Shiv <shivbirr@gmail.com>
2018-09-23 09:41:00 -0700

    finally corrected login..

--- Controllers/scripts/login.js
+++ Controllers/scripts/login.js
@@ -4,14 +4,12 @@ var token;
 var user;
 
 function myfunction(){
-    console.log("asdsadasdasdsadsdasdsadsa");
 
 var provider = new firebase.auth.GoogleAuthProvider();
 firebase.auth().signInWithPopup(provider).then(function(result) {
    token = result.credential.accessToken;
    user = result.user;
-  console.log(user);
-  console.log(token);
+ 
 }).catch(function(error) {
   var errorCode = error.code;
   var errorMessage = error.message;
