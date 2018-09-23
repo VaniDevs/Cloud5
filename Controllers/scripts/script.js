cb75b8b0a8b741040d814d5d2ad81f45c83c594a
Edmund Ham <hamsu2959@gmail.com>
2018-09-22 18:59:22 -0700

    added firebase auth

--- Controllers/scripts/script.js
+++ Controllers/scripts/script.js
@@ -1,4 +1,10 @@
-window.onload(function() {
-
-
-});
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDEtiyF_qJXqoN0BPv0KaOQ7LmiteTMLe4",
+    authDomain: "vanhack-eb10a.firebaseapp.com",
+    databaseURL: "https://vanhack-eb10a.firebaseio.com",
+    projectId: "vanhack-eb10a",
+    storageBucket: "vanhack-eb10a.appspot.com",
+      messagingSenderId: "872464678078"
+};
+firebase.initializeApp(config);
