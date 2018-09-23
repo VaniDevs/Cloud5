3d460e7a2bd1e82bda758c8d9f076d180d111ab5
prabhsaran <prabhvirsaran@gmail.com>
2018-09-23 10:19:49 -0700

    more ui changes

--- Controllers/scripts/admin.js
+++ Controllers/scripts/admin.js
@@ -1,13 +1,1 @@
-//var user = firebase.auth().currentUser;
-
-//if(user){
-
-//} else {
-    //document.write("Please login");
-//}
-
-import {getDataBaseConnection} from './firebaseConfig.js';
-
-let database = getDataBaseConnection();
-
-console.log(database);
+console.log("testing");
